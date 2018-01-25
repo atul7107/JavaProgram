a6d6756eab1504ca6d0a5b6f9045fe6332604260
atul7107 <atul.yadav7717@gmail.com>
2018-01-25 15:53:56 +0000

    1000 lines added

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
      public static void main(String[] arg){
 
 
-          for(int i=1;i<=100000;i++){
+          for(int i=1;i<=1000;i++){
 
                System.out.println("Hello World ..."+i);
 
